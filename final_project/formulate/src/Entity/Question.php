ce0ac225deca436da1e3bfca867c72affd6467e3
Sudipta Desktop BD <160587586+the-sudipta@users.noreply.github.com>
2025-06-22 00:24:55 +0600

    🚧 wip: Add Question entity and Question repository
    Created the Question entity to represent individual questions in the system and added a dedicated repository for handling question-related database operations. This supports future functionalities like templated question rendering or assessments.

---
+++ final_project/formulate/src/Entity/Question.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $showInResults = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $position = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isShowInResults(): ?bool
+    {
+        return $this->showInResults;
+    }
+
+    public function setShowInResults(bool $showInResults): static
+    {
+        $this->showInResults = $showInResults;
+
+        return $this;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+}
